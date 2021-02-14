970ef5132041419e923ceffbb6f7ae82291a3b6e
Sebastian Karlsen <sebastian@karlsen.fr>
2021-02-14 11:20:08 +0100

    Improve error handling

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+set -e
+
 for f in `find . -type f -not -path "./.git/*" -not -path "./deploy.sh" -not -path "./.gitignore" | cut -c3-`; do
 	rm ~/"$f"
 	cp "$f" ~/"$f"
 done
+
+exit 0
