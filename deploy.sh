fb16e51578848bdf252d262258836094a586aefe
Sebastian Karlsen <sebastian@karlsen.fr>
2021-03-02 13:35:34 +0100

    Comment code

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 for f in `find . -type f -not -path "./.git/*" -not -path "./deploy.sh" -not -path "./.gitignore" | cut -c3-`; do
+	# Only remove existing files (so we don't delete ~/.config by accident)
 	if [ -f ~/"$f" ]; then
 		rm ~/"$f"
 	fi
