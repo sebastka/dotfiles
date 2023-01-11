a5b6ddbaf82d4cb26f2a880f11cdf9b161fbeda9
Sebastian Karlsen <sebastian@karlsen.fr>
2023-01-11 04:18:51 +0100

    Fetch vim plugins with .chezmoiexternal

--- private_dot_config/vim/run_vimsetup.sh
+++ private_dot_config/vim/run_vimsetup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-[ ! -z "$XDG_STATE_HOME" ] || export XDG_STATE_HOME="$HOME/.config"
-[ ! -d "$XDG_STATE_HOME/vim/bundle/Vundle.vim" ] || exit 0
+## Moved to .chezmoiexternal.toml
 
-mkdir -p "$XDG_STATE_HOME/vim/bundle"
-git clone https://github.com/VundleVim/Vundle.vim.git "$XDG_STATE_HOME/vim/bundle/Vundle.vim"
-vim +PluginInstall +qall
+#[ ! -z "$XDG_STATE_HOME" ] || export XDG_STATE_HOME="$HOME/.config"
+#[ ! -d "$XDG_STATE_HOME/vim/bundle/Vundle.vim" ] || exit 0
+
+#mkdir -p "$XDG_STATE_HOME/vim/bundle"
+#git clone https://github.com/VundleVim/Vundle.vim.git "$XDG_STATE_HOME/vim/bundle/Vundle.vim"
+
+# https://github.com/VundleVim/Vundle.vim
+#vim +PluginInstall +qall
